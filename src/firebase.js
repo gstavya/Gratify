eab235dc1b49709a0213ede82e1e04c57f92859b
gstavya <137587109+gstavya@users.noreply.github.com>
2025-02-04 21:16:17 -0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAu5VHy8IyQmeTettqfui6I_zUGGdm-28w",
-    authDomain: "collegemash-fde92.firebaseapp.com",
-    projectId: "collegemash-fde92",
-    storageBucket: "collegemash-fde92.appspot.com",
-    messagingSenderId: "917282926405",
-    appId: "1:917282926405:web:5ff92a32cddd56ff7dd82c",
-    measurementId: "G-WYC9GM0JK8",
+   
 };
 
 // Initialize Firebase
